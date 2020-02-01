97336e0b0d00ebf9b748e14bff423bff4b47d048
Wuiyee Ho <wuiyee@fb.com>
2020-02-01 14:44:02 +0800

    - v1 conveyor belt

--- Scripts/GameScene.gd
+++ Scripts/GameScene.gd
@@ -8,10 +8,13 @@ var speed = 150
 var generate_every_x_seconds = 1.5
 var accumulator = 0
 var trash_position = 150
+var interval = 50
+
+onready var conveyor = get_node("Conveyor")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_gen_new_item()
+	_push_into_conveyor()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,8 +23,6 @@ func _process(delta):
 		_gen_new_item()
 	else:
 		accumulator += delta
-	for item in all_items:
-		item.position.x -= delta * speed
 	if all_items.size() != 0 && all_items.front().position.x <= trash_position:
 		_fall_into_trash(all_items.front())
 
@@ -32,19 +33,29 @@ func _input(event):
 				$Label.text = "Miss"
 			else:
 				$Label.text = "Good"
-			_fall_into_trash(all_items.front())
+			_next()
 		elif event.is_action_pressed("reject_item"):
 			if all_items.front().faulty:
 				$Label.text = "Good"
 			else:
 				$Label.text = "Miss"
-			_fall_into_trash(all_items.front())
+			_next()
+
+func _push_into_conveyor():
+	# for i in range(4):
+	var new_item = preloaded_item.instance()
+	_set_initial_position(new_item)
+	add_child(new_item)
+	conveyor._add(new_item)
 		
 func _gen_new_item():
 	var new_item = preloaded_item.instance()
 	all_items.push_back(new_item)
 	_set_initial_position(new_item)
 	add_child(new_item)
+
+func _next():
+	_push_into_conveyor()
 
 func _set_initial_position(item):
 	var position = window_size
--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,6 +1,10 @@
 extends Sprite
 
 var faulty = false
+var target_position = null
+var lerp_progress = 0.0
+var lerp_speed = 1.4
+var start_position = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +15,19 @@ func _ready():
 		faulty = true
 		modulate = Color(1,0,0)
 
+func _set_target_position(var target):
+	start_position = get_global_position()
+	target_position = target
+	lerp_progress = 0.0
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if target_position != null:
+		#transform = transform.interpolate_with(target_transform, lerp_progress);
+		var new_pos = start_position.linear_interpolate(target_position, lerp_progress)
+		set_global_position(new_pos)
+		lerp_progress += lerp_speed * delta
+		if lerp_progress >= 1.0:
+			lerp_progress = 1.0
+			target_position = null
